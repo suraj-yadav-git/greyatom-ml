c4bfc377538cfa800842e31e8ce8ae134040c3ee
Suraj Yadav <suraj.yadav@ingrammicro.com>
2020-09-19 16:04:33 +0530

    #4: Added summarized code functions

--- bin/summarizer.py
+++ bin/summarizer.py
@@ -1,4 +1,5 @@
 import yaml
+import numpy as np
 
 class SummarizeDoc:
 
@@ -18,5 +19,30 @@ class SummarizeDoc:
     def groupSentences(self, sentences):
         firstSent, restOfSent = sentences[0], sentences[1:]
         return firstSent, restOfSent
+
+    def findSentLength(self, text):
+        return text.split()
+
+    def findSentLengthArray(self, sentences):
+        return [self.findSentLength(sent) for sent in sentences]
+
+    def findTopSentence(self, sentLengths, sentences, n):
+        sortedIdx = np.argsort(sentLengths)
+        topIdx = sortedIdx[-n:]
+        topSentences = [sentences[i] for i in topIdx]
+        return topSentences
+
+    def fundSummary(self):
+        filePath = self.config['data_path']['train_data']
+        text = self.loadDoc(filePath)
+        sentences = self.splitSentences(text)
+        firstSent, restOfSent = self.groupSentences(sentences)
+        sentLengths = self.findSentLengthArray(restOfSent)
+        topSentences = self.findTopSentence(sentLengths, restOfSent, self.config['sentence_num'])
+        allSentences = firstSent + topSentences
+        summary = ' '.join(allSentences)
+        return summary
+
+
 
 summarizeObj = SummarizeDoc()
